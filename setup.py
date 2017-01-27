cffe9c3359fe01c5d8ab37853aa18076dfbe2b61
tal vintrob <tvaintrob@gmail.com>
2017-01-27 13:34:14 +0200

    made setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='meta music',
+      version='1',
+      description='search metadata for music files',
+      long_description='search metadata by fingerprinting the music file',
+      author='Tal Vintrob',
+      packages=['meta_music'],
+      license='MIT',
+      keywords=['meta', 'music', 'fingerprint'])
